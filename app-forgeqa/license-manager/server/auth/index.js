29c42e763308e82a4e8087bb0a52b770e969c09e
jagath132 <jagathranganathan@gmail.com>
2026-07-20 09:34:54 +0530

    Commit complete project folder to dev branch

--- app-forgeqa/license-manager/server/auth/index.js
+++
@@ -1,4 +0,0 @@
-export { adminStore } from "./store.js";
-export { authenticateToken, generateToken } from "./service.js";
-export { handleAuthRoute } from "./routes.js";
-export { logAudit, getAuditLogs } from "./audit.js";
